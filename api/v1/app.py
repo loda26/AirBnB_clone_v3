30b1e600d7acd8d5ddf10a129b59383cbda74cc4
loda26 <mokhaled26498@gmail.com>
2024-03-30 00:05:42 +0200

    task

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,23 +1,25 @@
-#!/use/bin/python3
-
+#!/usr/bin/python3
 """
 app
 """
 
 from flask import Flask
-from models import storage
-from api.v1.views import app_views
 from os import getenv
 
+from api.v1.views import app_views
+from models import storage
+
+
 app = Flask(__name__)
+
 
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def takedoen(exception):
+def teardown(exception):
     """
-    takedown fuction
+    teardown function
     """
     storage.close()
 
