e9222379634c07abd33b6321bc465993b94f00eb
loda26 <mokhaled26498@gmail.com>
2024-04-01 14:50:05 +0200

    task

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,9 +3,8 @@
 Method HTTP for States
 """
 
-from api.v1.views import app_views
+from api.v1.views import app_views, storage
 from flask import jsonify, abort, request
-from models import storage
 from models.state import State
 
 
