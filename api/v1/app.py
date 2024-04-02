da1c58bd92884f5a8a03cb26324fab0208526bf7
loda26 <mokhaled26498@gmail.com>
2024-04-02 17:41:24 +0200

    task

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,7 +13,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 
-CORS(app, resources={r"/*": {"origins": "127.0.0.1"}})
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 app.register_blueprint(app_views)
