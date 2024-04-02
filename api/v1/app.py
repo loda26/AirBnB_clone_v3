4a3b05fc1a0ef9ec18fe1ce0d8c53843f32ee7fb
loda26 <mokhaled26498@gmail.com>
2024-04-02 17:39:59 +0200

    task

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,12 +7,17 @@ from flask import Flask, jsonify
 from os import getenv
 from api.v1.views import app_views
 from models import storage
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 
 
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
 app.register_blueprint(app_views)
+# app.url_map.strict_slashes=False
 
 
 @app.teardown_appcontext
